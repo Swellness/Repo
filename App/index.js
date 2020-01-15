39ce7c1b3fcba711d92f16bb75421ba551a55fbb
Timothy Lancia <timothyjlancia@gmail.com>
2020-01-14 22:42:40 -0800

    1/14/2020 Tim Lancia Adding Screens to testBranch1

--- App/index.js
+++ App/index.js
@@ -1,19 +1,156 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import MainHub from "./screens/MainHub";
+import PostSession from "./screens/PostSession";
+import Activities from "./screens/Activities";
+import DailyHistory from "./screens/DailyHistory";
+import Leaderboard from "./screens/Leaderboard";
+import Profile from "./screens/Profile";
+import Rewards from "./screens/Rewards";
+import SessionHistory from "./screens/SessionHistory";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack"
+
+const AppNavigator = createStackNavigator({
+  MainHub:
+  {
+    screen: MainHub,
+    navigationOptions: {
+      headerTitle: "Main Menu",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      }
+    }
+  },
+  PostSession:
+  {
+    screen: PostSession,
+    navigationOptions: {
+      headerTitle: "",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  SessionHistory:
+  {
+    screen: SessionHistory,
+    navigationOptions: {
+      headerTitle: "Your Stats",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  DailyHistory:
+  {
+    screen: DailyHistory,
+    navigationOptions: {
+      headerTitle: "Stats",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  Rewards:
+  {
+    screen: Rewards,
+    navigationOptions: {
+      headerTitle: "Redeem",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  Leaderboard:
+  {
+    screen: Leaderboard,
+    navigationOptions: {
+      headerTitle: "Leaderboard",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  Activities:
+  {
+    screen: Activities,
+    navigationOptions: {
+      headerTitle: "Activites",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
+  Profile:
+  {
+    screen: Profile,
+    navigationOptions: {
+      headerTitle: "Profile",
+      headerBackTitle: "",
+      headerStyle:
+      {
+        backgroundColor: "#c2c5cc",
+      },
+      headerRight: () =>
+        <Button
+          onPress={() => alert('TBWO')}
+          title="Info"
+          color="#000000"
+        />,
+    }
+  },
 });
+
+export default createAppContainer(AppNavigator);
