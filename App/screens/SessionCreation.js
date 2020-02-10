edaa0e2942d5bcdcc78dccdb297a67ed5839fb4b
TheZinen <quizonjaylen@yahoo.com>
2020-02-09 19:42:29 -0800

    2/9/20 - Jaylen Quizon - Updated Navigation by component

--- App/screens/SessionCreation.js
+++ App/screens/SessionCreation.js
@@ -1,54 +1,105 @@
 import React from "react";
-import { View, ScrollView, StatusBar, TouchableOpacity, StyleSheet, onPress, Text, Button } from "react-native";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right,
+  Title,
+} from "native-base";
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'blue',
-    borderColor: 'white',
+    backgroundColor: "blue",
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 12,
-    color: 'white',
+    color: "white",
     fontSize: 24,
-    fontWeight: 'bold',
-    overflow: 'hidden',
+    fontWeight: "bold",
+    overflow: "hidden",
     padding: 12,
-    textAlign:'center',
+    textAlign: "center"
   },
   color: {
-    backgroundColor: 'blue',
-  },
+    backgroundColor: "blue"
+  }
 });
 
 export default class Start extends React.Component {
-render(){
+  render() {
+    return (
+      <Container>
+        <Header>
+          {/* <Left>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left> */}
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("ActiveSession")}
+          >
+            <Text style={styles.button}>Start Session</Text>
+          </TouchableOpacity>
 
-  return (
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("SideBar")}
+          >
+            <Text style={styles.button}>Side Menu</Text>
+          </TouchableOpacity>
 
-    <View>
-    <TouchableOpacity 
-    onPress={() => this.props.navigation.navigate("ActiveSession")} 
-    >
-      <Text style={styles.button}>Start Session</Text>
-    </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("SessionHistory")}
+          >
+            <Text style={styles.button}>History</Text>
+          </TouchableOpacity>
 
-    <TouchableOpacity 
-    onPress={() => this.props.navigation.navigate("SideBar")} 
-    >
-      <Text style={styles.button}>Side Menu</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity 
-    onPress={() => this.props.navigation.navigate("SessionHistory")} 
-    >
-      <Text style={styles.button}>History</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity 
-    onPress={() => this.props.navigation.navigate("Activities")} 
-    >
-      <Text style={styles.button}>Activities</Text>
-    </TouchableOpacity>
-    </View>
-        );
-    }
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Activities")}
+          >
+            <Text style={styles.button}>Activities</Text>
+          </TouchableOpacity>
+        </Content>
+        <Footer>
+          <FooterTab style={{ backgroundColor: "#c2c5cc" }}>
+            <Button
+              onPress={() => this.props.navigation.navigate("SessionHistory")}
+            >
+              <Icon name="calendar" style={{ color: "#000" }} />
+            </Button>
+            <Button
+              onPress={() => this.props.navigation.navigate("ActiveSession")}
+            >
+              <Icon active name="stopwatch" style={{ color: "#000" }} />
+            </Button>
+            <Button
+              onPress={() => this.props.navigation.navigate("Activities")}
+            >
+              <Icon name="heart" style={{ color: "#000" }} />
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 }
