6759deb542435e68d0393791c711566584e13f8f
jhonma7 <jeremy.honma@gmail.com>
2020-02-17 13:21:01 -0800

    database api created in util folder

---
+++ util/dbAPI.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Stitch, RemoteMongoClient, UserPasswordCredential, UserPasswordAuthProviderClient } from 'mongodb-stitch-react-native-sdk';
+
+
+//DOUCUMENTATION FOR MANIPULATING COLLECTIONS FOUND HERE
+//https://docs.mongodb.com/stitch-sdks/js-react-native/4/interfaces/remotemongocollection.html
+//WHEN YOU LOAD A COLLECTION, THE DB IS AUTOMATICALLY INCLUDED SO LONG AS YOU CORRECTLY NAME IT IN THE METHOD
+
+export const loadClient = () => { //ALWAYS NEEDED
+    Stitch.initializeDefaultAppClient('swellness-test-rkmwl')
+        .then(() => {
+            console.log("client built")
+        }).catch(err => {
+            console.log("load failed:", err)
+        })
+}
+
+export const hasDefaultClient = () => { //checks if client exists
+    if (Stitch.defaultAppClient === 'undefined') {
+        console.log("default client undefined")
+        return false
+    }
+    else {
+        console.log("default client defined")
+        return true
+    }
+}
+
+export const loadDb = (database) => { //returns RemoteMongoDatabase, almost never used
+    const db = this.state.client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db(database)
+    return db;
+}
+
+
+export const loadCollection = (database, collection) => { //returns RemoteMongoCollection, use this one
+    const ctn = Stitch.defaultAppClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db(database).collection(collection)
+    return ctn
+}
+
+export const login = (username, password) => { //logs in and returns user object
+    console.log("attempting login using  " + username + " and " + password)
+    Stitch.defaultAppClient.auth.loginWithCredential(new UserPasswordCredential(username, password)) //takes username and password combo and attempts email/pw auth through stitch
+        .then(user => {
+            console.log(`Successfully logged in as user ${user.profile.email}`);
+        }).catch(err => {
+            console.log(`Failed to log in ${err}`);
+        });
+}
+
+export const addData = (database, collection, input) => {
+    Stitch.defaultAppClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db(database).collection(collection).insertOne(input)
+        .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`)) //db.collection selects a collection and insertOne inserts the document and logs if successful or failure
+        .catch(err => console.error(`Failed to insert item: ${err}`))
+}
+//equivalent of taking a Stich colection object and applying the insertOne method
+
+
+export const updateData = (database, collection, objId, object) => {
+    Stitch.defaultAppClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db(database).collection(collection).updateOne({ objId }, { $set: object })
+}
+//equivalent of taking a Stich colection object and applying the upddateOne method
+
+
+//example object: const output = { "hours": data1, "points": data2 }
+
+
+updateData()
+
+export const logout = () => {
+    Stitch.defaultAppClient.auth.logout().then(user => { //takes the client and runs auth.logout() to log the client out
+        console.log(`Successfully logged out`);
+
+    }).catch(err => {
+        console.log(`Failed to log out: ${err}`);
+    });
+}
